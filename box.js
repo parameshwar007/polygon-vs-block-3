bfa4939757e42462044b0ea4ed8c6a97935afb63
parameshwar007 <71545961+parameshwar007@users.noreply.github.com>
2020-12-03 14:10:35 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,106 @@
+class Block {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.8,
+          'friction':1.0,
+          
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.visiblity=255;
+      World.add(world, this.body);
+    }
+    display(){
+
+Score()
+
+if(this.visibility<0 && this.visibility>-105){
+
+ score++; 
+
+ block1.score();
+ block2.score();
+ block3.score();
+ block4.score();
+ block5.score();
+ block6.score();
+ block7.score();
+ block8.score();
+ block9.score();
+ block10.score();
+ block11.score();
+ block12.score();
+ block13.score();
+ block14.score();
+ block15.score();
+ block16.score();
+
+ block1.score();
+ block2.score();
+ block3.score();
+ block4.score();
+ block5.score();
+ block6.score();
+ block7.score();
+ block8.score();
+ block9.score();
+
+
+}
+
+
+
+
+
+
+
+
+    console.log(this.body.speed)
+     if(this.body.speed<3){
+      super.display
+     } 
+      
+      else{
+
+World.remove(World,this.body)
+push()
+this.visiblity-this.visiblity-5;
+tint(255,this.visiblity)
+Image(this.image,this.body.position.X,this.position.Y,50,50)
+pop()
+
+
+
+      }
+      
+  
+      
+      
+      
+      
+  
+      var angle = this.body.angle;
+      var pos =this.body.position;
+
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      rect(0, 0, this.width, this.height);
+      pop();
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+  };
+  
